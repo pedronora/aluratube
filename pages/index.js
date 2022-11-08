d059cbc6e097ad68a820ed8081242ed0397fd0d0
Pedro Nora <pedro@nora.vc>
2022-11-08 14:58:54 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,83 @@
+import config from '../config.json';
+import styled from 'styled-components';
+import { CSSReset } from './components/CSSReset';
+import Menu from './components/Menu';
+import { StyledTimeline } from './components/Timeline';
+
+function HomePage() {
+    console.log(config.playlists);
+
+    return (
+        <>
+            <CSSReset />
+            <div>
+                <Menu />
+                <Header />
+                <Timeline playlists={config.playlists} />
+            </div>
+        </>
+    );
+}
+
+export default HomePage;
+
+// function Menu() {
+//     return <div>Menu</div>;
+// }
+
+const StyledHeader = styled.section`
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+
+    .user-info {
+        margin-top: 50px;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
+`;
+function Header() {
+    return (
+        <StyledHeader>
+            {/* <img src='banner' /> */}
+            <div className='user-info'>
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>{config.name}</h2>
+                    <p>{config.job}</p>
+                </div>
+            </div>
+        </StyledHeader>
+    );
+}
+
+function Timeline(props) {
+    const playlistsNames = Object.keys(props.playlists);
+    return (
+        <StyledTimeline>
+            {playlistsNames.map((playlistName) => {
+                const videos = props.playlists[playlistName];
+                return (
+                    <section>
+                        <h2>{playlistName}</h2>
+                        <div>
+                            {videos.map((video) => {
+                                return (
+                                    <a href={video.url}>
+                                        <img src={video.thumb} />
+                                        <span>{video.title}</span>
+                                    </a>
+                                );
+                            })}
+                        </div>
+                    </section>
+                );
+            })}
+        </StyledTimeline>
+    );
+}
